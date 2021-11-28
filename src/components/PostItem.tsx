c756e56313d4194c2395beeeda2e10e033c14d41
Johan Kohlin <johan.kohlin@moderskeppet.se>
2021-11-28 19:45:23 +0100

    added a tag pill next to recipe name

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -10,7 +10,7 @@ export default function PostItem({ post }: Props) {
   return (
     <Link href={"/posts/" + post.slug}>
       <a>
-        <h2 className="recept">{post.title}</h2>
+        <h2 className="recept">{post.title} <span className={'tag'}>{post.tags[0]}</span></h2>
         <style jsx>
           {`
             a {
@@ -20,6 +20,19 @@ export default function PostItem({ post }: Props) {
             h2 {
               margin: 0;
               font-weight: 500;
+            }
+            .tag {
+              font-weight: 300;
+              font-size: 0.8rem;
+              display: inline-block;
+              border: solid 1px #9AB9C0;
+              border-radius: 1rem;
+              padding: 0.4rem 0.7rem;
+              margin-left: 1rem;
+              color: #323D3F;
+              background-color: rgba(235, 235, 222, 0.14);
+              position: relative;
+              bottom: 5px;
             }
           `}
         </style>
